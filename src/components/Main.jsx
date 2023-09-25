8654a7ea8f43ab1f249b01aedbc2dd2147b1a8a7
Bhuwan <bhubanpaudel79@gmail.com>
2023-09-25 17:52:46 +0545

    try to make slide work@

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
-
+import banner from '../img/banner-1.jpg';
+import bg from '../img/bg.jpg';
 export default function Main() {
   return (
-    <div className="main z-10 pt-32 text-white font-sans">
+    <main>
+    <div className="main z-10 pt-32 text-white font-sans relative">
+
         <div className="wrap pl-20">
         <p className='uppercase text-sm font-bold pb-5 tracking-widest  '>Hey, there!</p>
         <h1 className='text-8xl w-[800px] font-bold pb-10'>View our delicious menu.</h1>
         <p className='w-96 text-lg text-gray-300 tracking-wide'>Our menu caters to a wide variety of palettes
             I'm sure you'll find something you love.</p>
-            <div className="button  font-semibold text-xs  flex gap-10 pt-20">
-                <button className='bg-green-700 tracking-widest px-4 py-4 rounded-lg'>RESERVATION</button>
+            <div className="button  relative font-semibold text-xs  flex gap-10 pt-20">
+              <button className=' reservation bg-green-700 tracking-widest px-5 py-4 rounded-lg'>RESERVATION
+               
+                </button>
+                
                 <button className='hover:bg-green-700 tracking-widest px-4 py-4 rounded-lg'>VIEW MENU</button>
+                
+          </div>
+            <div className=" transparent bg-white -mt-[1px]  w-[118px] ml-1 h-[7px]  opacity-20 rounded-xl">
+                </div>
             </div>
         </div>
-</div>
-        
+    </main>
   )
 }
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,17 +1,128 @@
 import Header from '../components/Header'
 import Main from '../components/Main';
 import bg from '../img/bg.jpg';
-export default function Home() {
+import banner1 from '../img/banner-1.jpg';
+import banner2 from '../img/banner-2.jpg';
+import banner3 from '../img/banner-3.jpg';
+import React, { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle as faCircleSolid } from '@fortawesome/free-solid-svg-icons';
+
+ function Slide1({ isVisible }) {
   return (
-    <>
-    <div className="main-home w-screen  grid" id='home'>
-      <img src={bg} alt="" className='relative' />
+    <div className={`slide1 w-screen grid transition-transform ${
+      isVisible ? 'translate-x-0 scale-110' : 'translate-y-[-100%] hidden'
+    }`} id='home'>
+      <div className="image relative">
+      <img src={bg} alt="" className='w-full h-auto' />
+      <img src={banner1} alt="" className='absolute -top-20 left-0  h-auto' />
+      </div>
       <div className="absolute">
        <Header/>
        <Main/>
       </div>
-    
     </div>
+    
+  )
+}
+ function Slide2({ isVisible }) {
+  return (
+    <div className= {`slide2 w-screen grid transition-transform ${
+      isVisible ? 'translate-x-0 scale-110' : 'translate-y-[-100%] hidden'
+    }`} id='home'>
+      <div className="image relative">
+      <img src={bg} alt="" className='w-full h-auto' />
+      <img src={banner2} alt="" className='absolute -top-20 left-0  h-auto' />
+      </div>
+      <div className="absolute">
+       <Header/>
+       <Main/>
+      </div>
+    </div>
+  )
+}
+ function Slide3({ isVisible }) {
+  return (
+    <div className={`slide3 w-screen grid transition-transform ${
+      isVisible ? 'translate-x-0 scale-110' : 'translate-y-[-100%] hidden'
+    }`} id='home'>
+      <div className="image relative">
+      <img src={bg} alt="" className='w-full h-auto' />
+      <img src={banner3} alt="" className='absolute -top-20 left-0  h-auto' />
+      </div>
+      <div className="absolute">
+       <Header/>
+       <Main/>
+      </div>
+    </div>
+  )
+}
+
+export default function Home() {
+  const slideData = [Slide1,Slide2,Slide3];
+   
+     const [activeSlide, setActiveSlide] = useState(0);
+     const [isPlaying, setIsPlaying] = useState(true);
+
+     useEffect(() =>{
+       let interval;
+       if(isPlaying){
+         interval = setInterval(() => {
+           setActiveSlide((prevSlide)  => (prevSlide+1) % slideData.length);
+           
+         }, 4000);
+       }else{
+         clearInterval(interval);
+       }
+       return  () => clearInterval(interval);
+     },[isPlaying,slideData.length]);
+   
+   
+     const handlePauseClick = () => {
+       setIsPlaying(false);
+     };
+     const handlePlayClick = () => {
+       setIsPlaying(true);
+     };
+     const handleCircleClick = (index) => {
+       setActiveSlide(index);
+     };
+      
+const renderCircles = () => {
+  return slideData.map((_, index) => (
+    <div key={index}
+    onClick={() => handleCircleClick(index)}
+     className="circle flex justify-between w-5 cursor-pointer "
+    >
+      <FontAwesomeIcon
+        icon={index === activeSlide ? faCircleSolid : faCircleSolid}
+        className={index === activeSlide ? 'solid-circle text-[9px] ' : 'solid-circle text-[9px] text-circle'}
+      />
+    </div>
+  ));
+ };
+     
+  return (
+    <>
+    <div className="main">
+    {slideData.map((Slide, index) => (
+          <Slide key={index} isVisible={index === activeSlide} />
+        ))}
+      
+    {/* {slideData[activeSlide]} */}
+    <div className="circle z-20 h-96 ">
+    {isPlaying ? (
+       <i class={`fa-solid fa-circle-pause text-2xl sm:text-3xl hover:scale-105 cursor-pointer transition-all pt-1 text-blue-600`}   onClick={handlePauseClick}></i>
+        ) : (
+         <i class="fa-solid fa-circle-play text-2xl sm:text-3xl hover:scale-105 cursor-pointer transition-all pt-1 text-blue-500" onClick={handlePlayClick}></i>
+                    )}
+            <div className="circle flex text-blue-500 h-20">
+                {renderCircles()}
+             </div>
+
+    </div>
+    </div>
+   
     
        
     </>
